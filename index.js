bc437e67cdb2d0b2307b786a50261aff02248595
Speykious <speykious.the.king@gmail.com>
2020-03-08 02:24:19 +0100

    Fixed the string regex + new version

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ bot.on('ready', () => {
 		.setTitle(`**MinesweeperBot** [v${version}]`)
 		.setColor(0x55ccff)
 		.addField('Owner my Lord:', `<@!${process.env.OWNER_ID}>`)
-		.addField('Last update:', 'Implemented a board cell hide/show/flag mechanism')
+		.addField('Last update:', 'Implemented a board cell hide/show/flag EDITING mechanism')
 		.setFooter(`I am now ON.`);
 
 	typing(defchan, testembed)
